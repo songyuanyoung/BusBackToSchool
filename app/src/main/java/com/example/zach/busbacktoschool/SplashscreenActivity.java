28acc7b4cd75a1d5d95e94a2709c97d440d15f88
songyuanyoung <songyuanyoung@gmail.com>
2017-07-12 10:35:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/zach/busbacktoschool/SplashscreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.zach.busbacktoschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
